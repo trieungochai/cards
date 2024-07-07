b864616ffc0f5d85a4900c9100cba7a39cd513bb
trieungochai <trieungochai.dev@gmail.com>
2024-07-08 00:48:23 +0700

    asserting elements in a slice

--- desk_test.go
+++ desk_test.go
@@ -8,4 +8,12 @@ func TestNewDesk(t *testing.T) {
 	if len(d) != 16 {
 		t.Errorf("Expected deck length of 16, but got %v", len(d))
 	}
+
+	if d[0] != "A of ♠" {
+		t.Errorf("Expected 1st card of A of ♠, but got %v", d[0])
+	}
+
+	if d[len(d)-1] != "4 of ♣" {
+		t.Errorf("Expected last card of 4 of ♣, but got %v", d[len(d)-1])
+	}
 }
