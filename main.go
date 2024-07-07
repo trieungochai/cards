3429897ced93def3df7f00b542379122087dd3b2
trieungochai <trieungochai.dev@gmail.com>
2024-07-07 11:12:50 +0700

    type conversion

--- main.go
+++ main.go
@@ -1,10 +1,16 @@
 package main
 
+import "fmt"
+
 func main() {
-	cards := newDeck()
+	// cards := newDeck()
 
-	hand, remainingCards := deal(cards, 5)
+	// hand, remainingCards := deal(cards, 5)
 
-	hand.print()
-	remainingCards.print()
+	// hand.print()
+	// remainingCards.print()
+
+	greeting := "See you Space Cowboy!"
+
+	fmt.Println([]byte(greeting))
 }
